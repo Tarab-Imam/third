778a5575f3cb238f152fabf6bbf9e70563b79b47
Tarab Imam <imamtarab@gmail.com>
2024-10-17 19:29:33 +0530

    Addn

---
+++ neww.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{ char d;
+  printf ("enter a character");
+  scanf ("%d",&d);
+  switch (d)  
+  {
+  case 'a':printf ("vowel");
+  case 'e':printf ("vowel");
+  case 'i':printf("vowel");
+  case 'o':printf('vowel');
+  case 'u':printf("vowel";)
+    
+    break;
+  
+  default:
+    break;
+  }}
