c32cdb14f97ae783b44b1c5ea06ace14cb9588e9
Tarab Imam <imamtarab@gmail.com>
2024-10-17 19:35:27 +0530

    ADD

--- neww.c
+++ neww.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 void main()
-{ char d;
-  printf ("enter a character");
-  scanf ("%d",&d);
+{ 
+   char d;
+   printf ("enter a character");
+   scanf ("%c",&d);
   switch (d)  
   {
   case 'a':printf ("vowel");
   case 'e':printf ("vowel");
   case 'i':printf("vowel");
   case 'o':printf('vowel');
-  case 'u':printf("vowel";)
+  case 'u':printf("vowel");
     
     break;
   
